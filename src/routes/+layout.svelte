230c02ac297ec2a3882170b241ac37e2ad11f96c
baggiest <mani.ehsanisohi@gmail.com>
2024-09-27 17:08:41 +0330

    forgot the selection color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import '../app.css';
 </script>
 
-<div class="min-h-screen bg-[#e7e3d7] text-[#2a2e58] text-center selection:bg-lime-500">
+<div class="min-h-screen bg-[#e7e3d7] text-[#2a2e58] text-center selection:bg-[#d42b41] selection:text-white">
 
 	<div class="flex items-center justify-center ">
 		<nav class="flex w-1/3 min-w-[330px] gap-6 px-2 py-8 text-xl text-[#2a2e58] font-semibold">
